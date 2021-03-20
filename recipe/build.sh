95ed83349899883361d0dae70445db35057f70f8
leej3 <leej3@quansight.com>
2021-03-20 11:04:12 +0000

    add jinja templating for a bootstrapping build
    if bootstrapping is set to yes in the conda_build_config.yaml, a constrained or intermediate build
    can be used that does not have the full set of build and testing dependencies typical for this feedstock

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
-export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
-export C_INCLUDE_PATH="${PREFIX}/include"
-
-# Legacy toolchain flags
-if [[ ${c_compiler} =~ .*toolchain.* ]]; then
-    if [ $(uname) == "Darwin" ]; then
-        export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
-        export CC=clang
-        export CXX=clang++
-    else
-        export LDFLAGS="$LDFLAGS -Wl,--disable-new-dtags"
-    fi
-fi
-if [[ ${target_platform} != osx-64 ]]; then
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,$PREFIX/lib"
+if [[ ! $BOOTSTRAPPING == yes ]]; then
+  # Get an updated config.sub and config.guess
+  cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 fi
 
-./configure \
-    --prefix=${PREFIX} \
-    --host=${HOST} \
-    --disable-ldap \
-    --disable-static \
-    --with-ca-bundle=${PREFIX}/ssl/cacert.pem \
-    --with-ssl=${PREFIX} \
-    --with-zlib=${PREFIX} \
-    --with-gssapi=${PREFIX} \
-    --with-libssh2=${PREFIX} \
-|| cat config.log
+# need macosx-version-min flags set in cflags and not cppflags
+export CFLAGS="$CFLAGS $CPPFLAGS"
+
+if [[ $BOOTSTRAPPING == yes ]]; then
+  ./configure \
+      --prefix=${PREFIX}
+else
+  ./configure \
+      --prefix=${PREFIX} \
+      --host=${HOST} \
+      --disable-ldap \
+      --with-ca-bundle=${PREFIX}/ssl/cacert.pem \
+      --with-ssl=${PREFIX} \
+      --with-ssl \
+      --with-zlib=${PREFIX} \
+      --with-gssapi=${PREFIX} \
+      --with-libssh2=${PREFIX} \
+      --with-nghttp2=${PREFIX} \
+  || cat config.log
+fi
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 # TODO :: test 1119... exit FAILED
