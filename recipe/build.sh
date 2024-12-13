76c0a488032794856d07b973d56d42585ccdb51c
Eric Lundby <Eric.Lundby@gmail.com>
2024-12-13 15:54:37 -0700

    v8.11.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,17 +13,26 @@ test -n "${AUTORECONF}" \
 # need macosx-version-min flags set in cflags and not cppflags
 export CFLAGS="$CFLAGS $CPPFLAGS"
 
+if [[ "$target_platform" == "osx-"* ]]; then
+    USESSL="--with-openssl=${PREFIX} --with-secure-transport --with-default-ssl-backend=openssl"
+else
+    USESSL="--with-openssl=${PREFIX}"
+fi
+
 ./configure \
     --prefix=${PREFIX} \
     --host=${HOST} \
     --disable-ldap \
     --disable-manual \
+    --enable-websockets \
     --with-ca-bundle=${PREFIX}/ssl/cacert.pem \
-    --with-ssl=${PREFIX} \
+    $USESSL \
     --with-zlib=${PREFIX} \
+    --with-zstd=${PREFIX} \
     --with-gssapi=${PREFIX} \
     --with-libssh2=${PREFIX} \
     --with-nghttp2=${PREFIX} \
+    --without-libpsl \
 || cat config.log
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
@@ -34,10 +43,8 @@ make -j${CPU_COUNT} ${VERBOSE_AT}
 # -am: automake style TAP output
 # -p: print logs if test fails
 # -j: parallelization
-# disable test 433, since it requires the glibc debug info
-# disable test 1173 and 1139 since --disable-manual is set
-# disable test 971, 1705, and 1706 because of locale settings on osx
-make TFLAGS="-v -a -k -p -j$(CPU_COUNT) !433 !1173 !1139 !971 !1705 !1706" test-nonflaky
+# disable test 1173 since --disable-manual is set
+make TFLAGS="-v -a -k -p -j$(CPU_COUNT) !1173" test-nonflaky
 
 make install
 
