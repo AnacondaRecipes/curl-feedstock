b5b90e529e0c2e105e349bd491178c7966d292d2
Onur Bingol <obingol@anaconda.com>
2026-01-27 10:01:45 -0800

    move similar CMake args together and remove duplicates

--- recipe/build.sh
+++ recipe/build.sh
@@ -19,17 +19,16 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DBUILD_SHARED_LIBS=ON \
     -DBUILD_STATIC_LIBS=OFF \
+    -DBUILD_TESTING=OFF \
+    -DBUILD_CURL_EXE=ON \
+    -DUSE_NGHTTP2=ON \
     -DCURL_CA_BUNDLE=${PREFIX}/ssl/cacert.pem \
     ${SSL_OPTIONS} \
-    -DCURL_USE_LIBSSH2=ON \
-    -DUSE_NGHTTP2=ON \
-    -DBUILD_CURL_EXE=ON \
     -DCURL_USE_LIBPSL=OFF \
     -DCURL_DISABLE_LDAP=ON \
     -DCURL_ZSTD=ON \
     -DCURL_USE_GSSAPI=ON \
     -DCURL_USE_LIBSSH2=ON \
-    -DBUILD_TESTING=OFF \
     ..
 
 cmake --build . --config ${BUILD_TYPE} --parallel ${CPU_COUNT} --target install --verbose
